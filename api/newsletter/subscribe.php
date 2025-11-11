703d80faa750eaf59379bd437a64379a418d33cc
Pinnacle96 <binary.rln@gmail.com>
2025-11-11 19:39:57 +0100

    new

--- api/newsletter/subscribe.php
+++ api/newsletter/subscribe.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../../config/security.php'; // provides url() and session
 require_once __DIR__ . '/../../config/db.php';
 require_once __DIR__ . '/../../includes/constants.php';
 
